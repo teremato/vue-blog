d2ea7b0ba1bb355aba449253d37753484c79d0bd
teremato <everosha.animation@gmail.com>
2022-09-02 00:24:44 +0500

    init project

---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="logo">Blog <span>Vue3</span></div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'app-header'
+}
+</script>
+
+<style lang="css" scoped>
+    .header {
+        background-color: rgba(255, 255, 255, 0.5);
+        box-shadow: 4px 14px 27px 0px rgba(34, 60, 80, 0.2);
+        color: gray;
+    }
+    .header span {
+        color: crimson;
+    }
+</style>
